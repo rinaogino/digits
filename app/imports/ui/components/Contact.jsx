a89f024cf0173a65d2917f5623aed646ba941729
rinaogino <rin4@hawaii.edu>
2024-03-30 21:47:46 -1000

    contact forms finished in 12 minutes.

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const Contact = ({ contact }) => (
@@ -18,6 +19,7 @@ const Contact = ({ contact }) => (
       <Card.Text>
         {contact.description}
       </Card.Text>
+      <Link to={`/edit/${contact._id}`}>Edit</Link>
     </Card.Body>
   </Card>
 );
@@ -30,7 +32,7 @@ Contact.propTypes = {
     lastName: PropTypes.string,
     address: PropTypes.string,
     description: PropTypes.string,
-    // _id: PropTypes.string,
+    _id: PropTypes.string,
   }).isRequired,
 };
 
